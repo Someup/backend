966d5d7f530f95a2795ef29a6f928123570cc9f1
junseoplee <junseoplee@soongsil.ac.kr>
2024-09-16 17:33:16 +0900

    chore: gitignore에 LocalSecurityConfig 추가 (#15)

--- src/main/java/project/backend/common/config/SecurityConfig.java
+++ src/main/java/project/backend/common/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,7 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import project.backend.common.auth.oauth.KakaoUserDetailsService;
+import project.backend.business.auth.service.oauth.KakaoUserDetailsService;
 import project.backend.common.auth.jwt.JwtAccessDeniedHandler;
 import project.backend.common.auth.jwt.JwtAuthenticationFailEntryPoint;
 import project.backend.common.auth.jwt.JwtFilter;
@@ -22,6 +23,7 @@ import project.backend.common.auth.jwt.JwtFilter;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@Profile("dev")
 public class SecurityConfig {
 
   private final JwtAuthenticationFailEntryPoint jwtAuthenticationFailEntryPoint;
@@ -41,7 +43,6 @@ public class SecurityConfig {
         .authorizeHttpRequests(request -> request
             .requestMatchers("/v1/auth/**").permitAll()
             .requestMatchers("/v1/exception/**").permitAll()
-            .requestMatchers("/h2-console/**").permitAll()
             .anyRequest().authenticated()
         )
         .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
