c1071ed26259e34e8a9038e06475871dde2660e6
junseoplee <junseoplee@soongsil.ac.kr>
2024-09-16 17:51:25 +0900

    refactor: 중복 로직 추출 (#15)

--- src/main/java/project/backend/common/auth/oauth/KakaoUserInfo.java
+++ src/main/java/project/backend/common/auth/oauth/KakaoUserInfo.java
@@ -18,38 +18,23 @@ public class KakaoUserInfo {
     this.attributes = attributes;
   }
 
-  public String getEmail() {
+  private Map<String, Object> getAccountAttribute(String key) {
     ObjectMapper objectMapper = new ObjectMapper();
-    TypeReference<Map<String, Object>> typeReferencer = new TypeReference<>() {
-    };
-
+    TypeReference<Map<String, Object>> typeReference = new TypeReference<>() {};
     Object kakaoAccount = attributes.get(KAKAO_ACCOUNT);
-    Map<String, Object> account = objectMapper.convertValue(kakaoAccount, typeReferencer);
+    Map<String, Object> account = objectMapper.convertValue(kakaoAccount, typeReference);
+    return objectMapper.convertValue(account.get(key), typeReference);
+  }
 
-    return (String) account.get(EMAIL);
+  public String getEmail() {
+    return (String) getAccountAttribute(EMAIL).get(EMAIL);
   }
 
   public String getName() {
-    ObjectMapper objectMapper = new ObjectMapper();
-    TypeReference<Map<String, Object>> typeReferencer = new TypeReference<>() {
-    };
-
-    Object kakaoAccount = attributes.get(KAKAO_ACCOUNT);
-    Map<String, Object> account = objectMapper.convertValue(kakaoAccount, typeReferencer);
-    Map<String, Object> profile = objectMapper.convertValue(account.get(PROFILE), typeReferencer);
-
-    return (String) profile.get(NAME);
+    return (String) getAccountAttribute(PROFILE).get(NAME);
   }
 
   public String getProfileImageUrl() {
-    ObjectMapper objectMapper = new ObjectMapper();
-    TypeReference<Map<String, Object>> typeReferencer = new TypeReference<>() {
-    };
-
-    Object kakaoAccount = attributes.get(KAKAO_ACCOUNT);
-    Map<String, Object> account = objectMapper.convertValue(kakaoAccount, typeReferencer);
-    Map<String, Object> profile = objectMapper.convertValue(account.get(PROFILE), typeReferencer);
-
-    return (String) profile.get(PROFILE_IMAGE_URL);
+    return (String) getAccountAttribute(PROFILE).get(PROFILE_IMAGE_URL);
   }
 }
