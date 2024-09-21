f01d1669f60b73b37ba8dfe54c422c163a48d562
junseoplee <junseoplee@soongsil.ac.kr>
2024-09-21 18:14:26 +0900

    refactor: API URI 변경 (#18)

--- src/main/java/project/backend/presentation/post/controller/PostController.java
+++ src/main/java/project/backend/presentation/post/controller/PostController.java
@@ -1,5 +1,6 @@
 package project.backend.presentation.post.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -17,53 +18,48 @@ import project.backend.presentation.post.dto.response.CreateUpdatePostResponse;
 import project.backend.presentation.post.dto.response.PostListResponse;
 import project.backend.presentation.post.dto.response.PostDetailResponse;
 
-import java.util.List;
-
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/post")
+@RequestMapping("/post")
 public class PostController {
 
-    private final PostService postService;
+  private final PostService postService;
 
-    @AssignCurrentUserInfo
-    @GetMapping
-    public ResponseEntity<PostListResponse> getPosts(CurrentUserInfo userInfo) {
-        List<PostListDto> posts = postService.getPostList(userInfo.getUserId());
-        PostListResponse response = new PostListResponse(posts);
+  @AssignCurrentUserInfo
+  @GetMapping
+  public ResponseEntity<PostListResponse> getPosts(CurrentUserInfo userInfo) {
+    List<PostListDto> posts = postService.getPostList(userInfo.getUserId());
+    PostListResponse response = new PostListResponse(posts);
+    return new ResponseEntity<>(response, HttpStatus.OK);
+  }
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
+  @AssignOrNullCurrentUserInfo
+  @PostMapping
+  public ResponseEntity<CreateUpdatePostResponse> createNewPost(CurrentUserInfo userInfo,
+      @RequestBody CreatePostRequest createPostRequest) {
+    Long postId = postService.createNewPostDetail(userInfo.getUserId(), createPostRequest.getUrl());
+    CreateUpdatePostResponse createUpdatePostResponse = new CreateUpdatePostResponse(postId);
+    return new ResponseEntity<>(createUpdatePostResponse, HttpStatus.CREATED);
+  }
 
+  @AssignCurrentUserInfo
+  @GetMapping("/{id}")
+  public ResponseEntity<PostDetailResponse> getPostDetailById(CurrentUserInfo userInfo,
+      @PathVariable("id") Long id) {
+    PostDetailDto postDetail = postService.getPostDetail(userInfo.getUserId(), id);
+    PostDetailResponse response = new PostDetailResponse(postDetail);
+    return new ResponseEntity<>(response, HttpStatus.OK);
+  }
 
-    @AssignOrNullCurrentUserInfo
-    @PostMapping
-    public ResponseEntity<CreateUpdatePostResponse> createNewPost(CurrentUserInfo userInfo,
-                                                                  @RequestBody CreatePostRequest createPostRequest) {
-        Long postId = postService.createNewPostDetail(userInfo.getUserId(), createPostRequest.getUrl());
-        CreateUpdatePostResponse createUpdatePostResponse = new CreateUpdatePostResponse(postId);
-        return new ResponseEntity<>(createUpdatePostResponse, HttpStatus.CREATED);
-    }
-
-    @AssignCurrentUserInfo
-    @GetMapping("/{id}")
-    public ResponseEntity<PostDetailResponse> getPostDetailById(CurrentUserInfo userInfo,
-                                                                @PathVariable("id") Long id) {
-
-        PostDetailDto postDetail = postService.getPostDetail(userInfo.getUserId(), id);
-        PostDetailResponse response = new PostDetailResponse(postDetail);
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
-
-    @AssignCurrentUserInfo
-    @PatchMapping("/{id}")
-    public ResponseEntity<CreateUpdatePostResponse> updatePost(CurrentUserInfo userInfo,
-                                                               @PathVariable("id") Long postId,
-                                                               @RequestBody UpdatePostRequest updatePostRequest) {
-        Long id = postService.updatePostDetail(userInfo.getUserId(), postId, updatePostRequest.toServiceDto());
-        CreateUpdatePostResponse createUpdatePostResponse = new CreateUpdatePostResponse(id);
-        return new ResponseEntity<>(createUpdatePostResponse, HttpStatus.OK);
-    }
+  @AssignCurrentUserInfo
+  @PatchMapping("/{id}")
+  public ResponseEntity<CreateUpdatePostResponse> updatePost(CurrentUserInfo userInfo,
+      @PathVariable("id") Long postId,
+      @RequestBody UpdatePostRequest updatePostRequest) {
+    Long id = postService.updatePostDetail(userInfo.getUserId(), postId,
+        updatePostRequest.toServiceDto());
+    CreateUpdatePostResponse createUpdatePostResponse = new CreateUpdatePostResponse(id);
+    return new ResponseEntity<>(createUpdatePostResponse, HttpStatus.OK);
+  }
 }
