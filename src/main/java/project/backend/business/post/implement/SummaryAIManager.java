f300dcc2de511ec1be2220393af7e3eba8df7b1a
‍김용빈 <yongbin97@yonsei.ac.kr>
2024-09-21 17:43:04 +0900

    chore: 프롬프트에 옵션 추가 (#5)

--- src/main/java/project/backend/business/post/implement/SummaryAIManager.java
+++ src/main/java/project/backend/business/post/implement/SummaryAIManager.java
@@ -8,6 +8,7 @@ import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatModel;
 import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatOptions;
 import org.springframework.stereotype.Component;
 import project.backend.business.post.dto.CreatePostDto;
+import project.backend.business.post.dto.summary.SummaryLanguage;
 
 @Slf4j
 @Component
@@ -27,10 +28,12 @@ public class SummaryAIManager {
 
 
   private Prompt getPrompt(CreatePostDto createPostDto) {
-    String requestMessage = ("URL: %s\n" +
-        "위 웹사이트를 블로그 형태로 요약해줘.\n" +
-        "길이는 10줄 정도로,\n" +
-        "말투는 '~합니다'가 아닌 '~한다'의 형태로 해줘").formatted(createPostDto.getUrl());
+    String requestMessage = "URL: " + createPostDto.getUrl() + "\n" +
+        "위 웹사이트를 요약 조건에 맞춰서 블로그 형태로 요약해줘. 출처도 명시해줘!\n"+
+        "요약조건: \n" +
+        "1. 요약 길이: " + createPostDto.getOption().getLevel().getLines() + "\n" +
+        "2. 요약 말투: " + createPostDto.getOption().getTone().getValue() +
+        "3. 요약 언어: " + createPostDto.getOption().getLanguage().getValue();
 
     return new Prompt(requestMessage,
         VertexAiGeminiChatOptions.builder()
