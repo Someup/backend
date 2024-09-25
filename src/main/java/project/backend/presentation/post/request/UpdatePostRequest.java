973acbec16a915c2dafc0b04b73304a8a135188e
junseoplee <junseoplee@soongsil.ac.kr>
2024-09-25 19:32:58 +0900

    refactor: PostRequest 유효성 검사 업데이트 (#25)

--- src/main/java/project/backend/presentation/post/request/UpdatePostRequest.java
+++ src/main/java/project/backend/presentation/post/request/UpdatePostRequest.java
@@ -20,7 +20,6 @@ public class UpdatePostRequest {
   @Size(max = 5, message = "해시태그는 최대 5개까지 입력할 수 있습니다.")
   private List<String> tagList;
 
-  @Min(value = 1, message = "저장할 아카이브를 지정해야 합니다.")
   private int archiveId;
 
   @Size(max = 2000, message = "메모는 2000자를 초과할 수 없습니다.")
