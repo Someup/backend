1416aa437846581ae904c746654276ac7bb45e64
junseoplee <junseoplee@soongsil.ac.kr>
2024-09-26 21:38:18 +0900

    chore: 에러 상태 코드 변경 (#29)

--- src/main/java/project/backend/common/error/ErrorCode.java
+++ src/main/java/project/backend/common/error/ErrorCode.java
@@ -13,7 +13,7 @@ public enum ErrorCode {
   INVALID_ACCESS_TOKEN("유효하지 않은 엑세스 토큰입니다.", HttpStatus.UNAUTHORIZED),
   TOKEN_INVALID("유효하지 않은 토큰입니다.", HttpStatus.UNAUTHORIZED),
   ACCESS_DENIED("접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
-  NOT_EXIST_REFRESH_TOKEN("존재하지 않는 리프레시 토큰입니다.", HttpStatus.UNAUTHORIZED);
+  NOT_EXIST_REFRESH_TOKEN("존재하지 않는 리프레시 토큰입니다.", HttpStatus.BAD_REQUEST);
   private final String message;
   private final HttpStatus httpStatus;
 
